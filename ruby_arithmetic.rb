513507e122c1557b5631b96371d91229cdd2331d
Shubham Rajput <rajput.shubh1993@gmail.com>
2021-09-04 21:56:37 +0530

    Order of operations in Ruby Arithmetic

---
+++ ruby_arithmetic.rb
@@ -0,0 +1,11 @@
+# Order of Operations in Ruby Arithmetic
+# BODMAS
+# (Brackets, Orders, Multiplication, Addition and Substraction)
+
+5+15*20-2/6**3-(3+1)
+5+15*20-2/6**3-4
+5+15*20-2/216-4
+5+15*20-0-4
+5+300-4
+305-4
+301
