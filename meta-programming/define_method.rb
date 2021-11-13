3c65b31ab8d518ffb97a23e447a7bddb9df11988
Shubham Rajput <rajput.shubh1993@gmail.com>
2021-11-13 17:28:04 +0530

    Create method dynamically using define_methhod

---
+++ meta-programming/define_method.rb
@@ -0,0 +1,21 @@
+## DynamicallyMethodCreation
+
+class Book
+
+  genres = %w{fiction horror history}
+
+  genres.each do |genre|
+    define_method("#{genre}_details") do |args|
+      puts "Genre : #{genre}"
+      puts args
+      puts genre.object_id
+    end
+  end
+end
+
+book = Book.new
+book.history_details("Sir Richard")
+book.horror_details("Shubham")
+book.fiction_details("Kelly")
+
+p book.respond_to?(:fiction_details) # true
