b950f55c866ebabde64b08f28b72906180931add
Shubham Rajput <rajput.shubh1993@gmail.com>
2022-01-16 21:33:29 +0530

    power digit sum

---
+++ common-problems/power_digit_sum.rb
@@ -0,0 +1,9 @@
+## PowerDigitSum
+
+1. 2**5 = 32 and the sum of its digit is 3 + 2 = 5
+  What is the sum of digits of the number 5**5 ?
+
+sol :- 
+ result = 5**5  # 3125
+ result.to_s.split("").map(&:to_i).sum
+ #=>3+1+2+5 = 11
