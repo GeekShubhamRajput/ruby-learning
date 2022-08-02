fade36bfbbe0d7eec2219c9939307f30014fd204
Shubham Rajput <rajput.shubh1993@gmail.com>
2022-08-02 16:54:12 +0530

    Calculate lexicographic permutations

---
+++ common-problems/lexical_permutations.rb
@@ -0,0 +1,9 @@
+## LexicographicPermutations
+
+Prob. The Lexicographic permutation of 0, 1, 2 are:
+012, 021, 102, 120, 201, 210
+
+What is the millionth Lexicographic permutations of digit 0,  1,  2,  3,  4,  5,  6,  7,  8 and 9 ?
+
+sol. 
+p [0,  1,  2,  3,  4,  5,  6,  7,  8, 9].permutation.to_a[999_999].join
