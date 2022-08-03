7338a400535cba11a01872f74e4476689ea41bf8
Shubham Rajput <rajput.shubh1993@gmail.com>
2022-08-03 16:05:15 +0530

    Find Least Common Multiple

---
+++ common-problems/least_common_multiple.rb
@@ -0,0 +1,9 @@
+## LeastCommonMultiple
+
+prob. 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+sol.
+
+p (1..20).to_a.reduce(:lcm)
