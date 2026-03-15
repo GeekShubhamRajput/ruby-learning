9331ff4cbbf90dfb7ed9c3f181532ac3cd00dd7b
Shubham Rajput <rajput.shubh1993@gmail.com>
2026-03-15 18:43:47 +0530

    The puts method

---
+++ basics/puts_method.rb
@@ -0,0 +1,10 @@
+puts 1 3 5
+# this code throws unexpected tINTEGER error.
+# In Ruby, arguments must be separated by commas (or passed differently).
+# Writing 1 3 5 makes Ruby think you wrote multiple expressions without separators
+
+puts 1, 3, 5
+# This code will code
+# 1
+# 3
+# 5
