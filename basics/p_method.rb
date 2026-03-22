212edb855fcf8ac722d75df57964898e8490cd4c
Shubham Rajput <rajput.shubh1993@gmail.com>
2026-03-22 15:26:17 +0530

    Ruby p method

---
+++ basics/p_method.rb
@@ -0,0 +1,13 @@
+# p method
+
+p "Hello, Good morning"
+# => "Hello, Good morning"
+
+p "5"
+# => "5"
+
+p 5
+# => 5
+
+p "Hello \n new line start from here"
+# => "Hello \n new line start from here"
