7745f5a0ac116b85bc84071f7683751d4bcfa0ed
Shubham Rajput <rajput.shubh1993@gmail.com>
2026-04-05 23:12:27 +0530

    Ruby block methods

---
+++ basics/ruby_block_methods.rb
@@ -0,0 +1,13 @@
+## Ruby Block Methods
+
+# times
+3.times { |count| puts count }
+
+# upto
+1.upto(5) { |count| puts count }
+
+# downto
+5.downto(3) { |count| puts count }
+
+# step
+1.step(20, 7) { |count| puts count }
