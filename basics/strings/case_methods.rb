a698ec4bb5f9c974cdd773d9fbc7876e6e932000
Shubham Rajput <rajput.shubh1993@gmail.com>
2026-04-19 11:06:44 +0530

    Case methods

---
+++ basics/strings/case_methods.rb
@@ -0,0 +1,18 @@
+## Case Methods
+# None of below methods change original string instead create a new modified string.
+
+## upcase
+puts "Ram temple".upcase
+=> RAM TEMPLE
+
+## downcase
+puts "Ram Temple".downcase
+=> ram temple
+
+## swapcase
+puts "Ram Temple".swapcase
+=> rAM tEMPLE
+
+## capitalize
+puts "ram temple".capitalize 
+=> Ram temple
