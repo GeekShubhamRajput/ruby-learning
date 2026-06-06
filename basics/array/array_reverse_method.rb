a19f007de61aadda7c74bc4dd0f781976cffcd1f
Shubham Rajput <rajput.shubh1993@gmail.com>
2026-06-06 13:05:27 +0530

    Ruby Array reverse Method

---
+++ basics/array/array_reverse_method.rb
@@ -0,0 +1,15 @@
+## Array reverse Method
+
+# reverse Method
+Returns a new array containing the elements of self in reverse order:
+
+reverse → new_array
+
+[0, 1, 2].reverse # => [2, 1, 0]
+
+# reverse! Method
+reverse! → self
+
+a = [0, 1, 2]
+a.reverse! # => [2, 1, 0]
+a          # => [2, 1, 0]
