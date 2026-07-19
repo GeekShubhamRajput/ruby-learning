8df751ff421a84c99498a6885b3d24d917927fd5
Shubham Rajput <rajput.shubh1993@gmail.com>
2026-07-19 17:20:08 +0530

    Multi-Level inheritance

--- oops/inheritance_in_ruby.rb
+++ oops/inheritance_in_ruby.rb
@@ -1,25 +1,30 @@
 ## Inheritance in Ruby
 
-# Inheritance allows the programmer to inherit the characteristics of one class into another class.
+# Inheritance is an OOP feature that allows a child class to inherit the properties
+# and methods of a parent class. It promotes code reusability and allows the child
+# to extend or override the parent's behavior.
+# Ruby supports single inheritance and multilevel inheritance (but not multiple inheritance).
 # SubClass < SuperClass
 
-class MobilePhone
-  def calling
-    puts "I am calling..."
+class Animal
+  def breathe
+    "Breathing"
   end
 end
 
-class NokiaPhone < MobilePhone
+class Mammal < Animal
+  def walk
+    "Walking"
+  end
 end
 
-class SamsungPhone < MobilePhone
+class Dog < Mammal
+  def bark
+    "Barking..."
+  end
 end
 
-mobile_phone = MobilePhone.new
-mobile_phone.calling
-
-nokia_phone = NokiaPhone.new
-nokia_phone.calling
-
-samsung_phone = SamsungPhone.new
-samsung_phone.calling 
+dog = Dog.new
+puts dog.breathe   # Breathing
+puts dog.walk      # Walking
+puts dog.bark      # Barking...
