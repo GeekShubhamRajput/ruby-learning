7617be108501150a09f872a197fbd31f775612c9
Shubham Rajput <rajput.shubh1993@gmail.com>
2026-08-09 22:58:43 +0530

    Math Module

---
+++ basics/modules-and-mixins/math_module.rb
@@ -0,0 +1,11 @@
+## Math Module
+# Module Math provides methods for basic trigonometric, logarithmic, and transcendental
+# functions, and for extracting roots.
+
+puts Math.class    # Module
+
+puts Math.sqrt(4)  # 2.0
+
+puts Math.sqrt(5)  # 2.23606797749979
+
+puts Math::PI      # 3.141592653589793
