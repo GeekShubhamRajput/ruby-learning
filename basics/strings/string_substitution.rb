c70132fc1cc975d94e6d5fa94662c5e59e1ef900
Shubham Rajput <rajput.shubh1993@gmail.com>
2026-08-29 17:28:21 +0530

    Ruby sub and gsub Methods

--- basics/strings/string_substitution.rb
+++ basics/strings/string_substitution.rb
@@ -1,9 +1,25 @@
-# sub() vs gsub()
+## sub and gsub() Method
+
+## sub Method
+sub(pattern, replacement) → new_string
+sub(pattern) {|match| ... } → new_string
+
+# One substitution (or none); returns a new string.
 
 str = "The quick brown fox jumped over the quick dog"
 p str.sub("quick", "slow")
-=> "The slow brown fox jumped over the quick dog"
+# "The slow brown fox jumped over the quick dog"
+
+## gsub Method
+gsub(pattern, replacement) → new_string
+gsub(pattern) {|match| ... } → new_string
+gsub(pattern) → enumerator
+
+# Zero or more substitutions; returns a new string.
 
 str = "The quick brown fox jumped over the quick dog"
 p str.gsub("quick", "slow")
-=> "The slow brown fox jumped over the slow dog"
+# "The slow brown fox jumped over the slow dog"
+
+s = 'abracadabra'
+s.gsub(/[a-c]/, 'X')  # "XXrXXXdXXrX"
